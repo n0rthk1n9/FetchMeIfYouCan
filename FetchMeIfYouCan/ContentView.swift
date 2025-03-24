6194c9aaa8b2692b9a6bd40ec7af3f3e19ca711e
Jan Armbrust <jan.armbrust@googlemail.com>
2025-03-24 19:39:53 +0100

    Starter template

---
+++ FetchMeIfYouCan/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  FetchMeIfYouCan
+//
+//  Created by Jan Armbrust on 24.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Because who needs a pilot’s license when you have URLSession")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                NavigationLink("Stage 1: Get All Characters", destination: EmptyView())
+                NavigationLink("Stage 2: Search Characters by House", destination: EmptyView())
+                NavigationLink("Stage 3: Clean Architecture", destination: EmptyView())
+                Spacer()
+            }
+            .navigationTitle("Fetch Me If You Can")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FetchMeIfYouCan/FetchMeIfYouCanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMeIfYouCanApp.swift
+//  FetchMeIfYouCan
+//
+//  Created by Jan Armbrust on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FetchMeIfYouCanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
